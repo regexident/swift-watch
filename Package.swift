5b749a40483ac43abe826c3ee0b33ae2d79d892c
Vincent Esche <regexident@gmail.com>
2017-11-26 19:09:20 +0100

    Specified swift version as v4

--- Package.swift
+++ Package.swift
@@ -1,21 +1,17 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-watch",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/JohnSundell/Files.git", from: "2.0.0"),
         .package(url: "https://github.com/daniel-pedersen/SKQueue.git", from: "1.1.0"),
         .package(url: "https://github.com/surfandneptune/CommandCougar.git", from: "1.0.0"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
+//        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
+        .package(url: "https://github.com/regexident/Rainbow", .branch("manifest")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "swift-watch",
             dependencies: ["Files", "SKQueue", "CommandCougar", "Rainbow"]),
