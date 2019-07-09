56cc54b6d01102723d3106a2ded4c408e574a684
Vincent Esche <regexident@gmail.com>
2019-07-09 19:20:59 +0200

    Updated dependencies

--- Package.swift
+++ Package.swift
@@ -5,14 +5,15 @@ import PackageDescription
 let package = Package(
     name: "swift-watch",
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Files.git", from: "2.0.0"),
-        .package(url: "https://github.com/daniel-pedersen/SKQueue.git", from: "1.1.0"),
-        .package(url: "https://github.com/surfandneptune/CommandCougar.git", from: "1.0.0"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.3"),
+        .package(url: "https://github.com/JohnSundell/Files", from: "3.1.0"),
+        .package(url: "https://github.com/daniel-pedersen/SKQueue", from: "1.2.0"),
+        .package(url: "https://github.com/surfandneptune/CommandCougar", from: "1.1.1"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.0"),
     ],
     targets: [
         .target(
             name: "swift-watch",
-            dependencies: ["Files", "SKQueue", "CommandCougar", "Rainbow"]),
+            dependencies: ["Files", "SKQueue", "CommandCougar", "Rainbow"]
+        ),
     ]
 )
