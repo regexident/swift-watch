9e24dd519c55c70aa1db741fe331fb0092c3a549
Vincent Esche <regexident@gmail.com>
2023-12-27 00:25:13 +0100

    Improve 'Package.swift' file, adding an executable product

--- Package.swift
+++ Package.swift
@@ -1,9 +1,13 @@
-// swift-tools-version:4.2
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "swift-watch",
+    platforms: [
+        .macOS(.v10_15),
+    ],
     products: [
         .executable(
             name: "swift-watch",
@@ -31,7 +35,7 @@ let package = Package(
         ),
     ],
     targets: [
-        .target(
+        .executableTarget(
             name: "swift-watch",
             dependencies: [
                 "Files",
