cb4742c91879225974d29b4423d1ef0c972334c9
Vincent Esche <regexident@gmail.com>
2023-12-27 00:25:13 +0100

    Improve 'Package.swift' file, adding an executable product

--- Package.swift
+++ Package.swift
@@ -4,16 +4,41 @@ import PackageDescription
 
 let package = Package(
     name: "swift-watch",
+    products: [
+        .executable(
+            name: "swift-watch",
+            targets: [
+                "swift-watch",
+            ]
+        ),
+    ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/Files", from: "3.1.0"),
-        .package(url: "https://github.com/daniel-pedersen/SKQueue", from: "1.2.0"),
-        .package(url: "https://github.com/surfandneptune/CommandCougar", from: "1.1.1"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.0"),
+        .package(
+            url: "https://github.com/JohnSundell/Files",
+            from: "3.1.0"
+        ),
+        .package(
+            url: "https://github.com/daniel-pedersen/SKQueue",
+            from: "1.2.0"
+        ),
+        .package(
+            url: "https://github.com/surfandneptune/CommandCougar",
+            from: "1.1.1"
+        ),
+        .package(
+            url: "https://github.com/onevcat/Rainbow",
+            from: "3.1.0"
+        ),
     ],
     targets: [
         .target(
             name: "swift-watch",
-            dependencies: ["Files", "SKQueue", "CommandCougar", "Rainbow"]
+            dependencies: [
+                "Files",
+                "SKQueue",
+                "CommandCougar",
+                "Rainbow",
+            ]
         ),
     ]
 )
